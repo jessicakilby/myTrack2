7f9667bf1830c8dbcbfe6327a7ec56961bc05faa
jessica kilby <jessicakilby@gmail.com>
2017-06-10 16:22:48 -0500

    models

--- myTrack/Models/Category.cs
+++ myTrack/Models/Category.cs
@@ -8,10 +8,12 @@ namespace myTrack.Models
 {
     public class Category
     {
+
         [Key]
         public int CatId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public List<SubCategory> SubCategories { get; set; }
+        public List<Subcategory> SubCategories { get; set; }
+
     }
 }
--- myTrack/Models/Item.cs
+++ myTrack/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,8 @@ namespace myTrack.Models
 {
     public class Item
     {
+
+        [Key]
         public int ItemId { get; set; }
         public string Title { get; set; }
         public bool Status { get; set; }
