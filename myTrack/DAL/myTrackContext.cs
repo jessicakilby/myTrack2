d0a01025560219109167ff0d70e2aa3640cf8fd1
jessica kilby <jessicakilby@gmail.com>
2017-06-10 16:35:27 -0500

    myTrackContext

---
+++ myTrack/DAL/myTrackContext.cs
@@ -0,0 +1,18 @@
+﻿using myTrack.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace myTrack.DAL
+{
+    public class myTrackContext : DbContext
+    {
+        public myTrackContext : base("myTrack") {}
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Subcategory> Subcategories { get; set; }
+        public virtual DbSet<Item> Items { get; set; }
+    }
+}
