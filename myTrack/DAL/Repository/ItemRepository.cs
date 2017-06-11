5e477221d4b072cca61d0dcf9b60a48ec0bfd44a
jessica kilby <jessicakilby@gmail.com>
2017-06-10 19:03:27 -0500

    implement interfaces in repositories

--- myTrack/DAL/Repository/ItemRepository.cs
+++ myTrack/DAL/Repository/ItemRepository.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using myTrack.Controllers.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using myTrack.Models;
 
 namespace myTrack.DAL.Repository
 {
-    public class ItemRepository
+    public class ItemRepository : IItemRepository
     {
+        public void AddItem(Item newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteItem(int deleteItemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditItem(Item editItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetSingleItem(int ItemId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- myTrack/DAL/Repository/SubcategoryRepository.cs
+++ myTrack/DAL/Repository/SubcategoryRepository.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using myTrack.Controllers.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using myTrack.Models;
 
 namespace myTrack.DAL.Repository
 {
-    public class SubcategoryRepository
+    public class SubcategoryRepository : ISubcategoryRepository
     {
+        public void AddSubcategory(Subcategory newSubcategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteSubcategory(int deleteSubcatId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditSubcategory(Subcategory editSubcategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Subcategory> GetAllSubcategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Subcategory GetSingleSubcategory(int SubcatId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
