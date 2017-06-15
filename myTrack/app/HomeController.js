e7f085404e905ebd666d7504c8d1763afa400d2a
jessica kilby <jessicakilby@gmail.com>
2017-06-15 17:31:16 -0500

    working on adding categories

--- myTrack/app/HomeController.js
+++ myTrack/app/HomeController.js
@@ -7,6 +7,30 @@
             $http.defaults.headers.common['Authorization'] = "";
 
             $location.path("/login");
+        };
+
+        $scope.addCategory = function () {
+            console.log("clicked add category button");
+
+            $scope.categoryTitle = "";
+            $scope.categoryDescription = "";
+
+            
+        };
+
+        $scope.postAddCategory = function () {
+            console.log("clicked post add category after modal popup");
+
+            $http({
+                method: 'POST',
+                url: "",
+                headers: {},
+                data: {
+                    title: $scope.categoryTitle,
+                    Description : $scope.categoryDescription
+                }
+            })
+
         }
     }
 ]);
---
+++ myTrack/app/SubcategoryController.js
@@ -0,0 +1,10 @@
+﻿app.controller("SubcategoryController", ["$scope", "$http", "$location",
+    function ($scope, $http, $location) {
+
+        $scope.addSubcategory = function () {
+            console.log("clicked add subcategory controller");
+        };
+
+       
+    }
+]);
