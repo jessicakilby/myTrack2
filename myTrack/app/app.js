343c9d4d19109170e8012b95260ae69c5273e55b
jessica kilby <jessicakilby@gmail.com>
2017-06-15 15:56:29 -0500

    front end work

--- myTrack/app/app.js
+++ myTrack/app/app.js
@@ -20,17 +20,17 @@ app.config(["$routeProvider", function ($routeProvider) {
         {
             templateUrl: "app/Partials/Login.html",
             controller: "LoginController"
+        })
+        .when("/subcategory",
+        {
+            templateUrl: "app/Partials/Subcategory.html",
+            controller: "SubcategoryController"
+        })
+        .when("/item",
+        {
+            templateUrl: "app/Partials/Item.html",
+            controller: "ItemController"
         });
-        //.when("/subcategory",
-        //{
-        //    templateUrl: "app/Partials/Subcategory.html",
-        //    controller: "SubcategoryController"
-        //})
-        //.when("/item",
-        //{
-        //    templateUrl: "app/Partials/Item.html",
-        //    controller: "ItemController"
-        //});
         //.when("/login",
         //{
         //    templateUrl: "app/Partials/Login.html",
